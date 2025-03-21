e91d9a0220f48253df7cca5fbf83afe17673139e
shc <sihn7770@gmail.com>
2025-03-21 17:33:41 +0900

    sih_commit

--- teamproject/src/pages/Home.jsx
+++ teamproject/src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
             <Header />
           </HeaderWrapper>
           <br />
-          <Banner />
+          <Banner/>
           <CategoryMenu />
           <NoticeBox />
           <CategoryBanner />
